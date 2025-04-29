f716a44ec0ab9e5cce9562890f61e02bc9687240
Avash <133788295+AavasShrestha@users.noreply.github.com>
2025-04-29 21:11:27 +0545

    Added List of Task

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='overflow-x-auto h-[55%] flex items-center justify-left gap-5 flex-nowrap w-full py-5'>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl p-5'> 
+        <div className='flex justify-between '>
+          <h3 className='bg-red-700 px-3 py-1 rounded '>High</h3>
+          <h4 className='font-semibold'>20 Jan 2025</h4>
+        </div>
+
+        <h2 className='mt-5 text-xl font-semibold'> Make a new Project</h2>
+        <p className='text-sm'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, libero voluptatem. 
+          Distinctio ullam similique obcaecati dolore harum 
+          laborum non maiores temporibus illum assumenda illo quis, 
+          quisquam aut praesentium veritatis. Temporibus.
+            </p>
+
+      </div>
+
+
+      <div className='flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl p-5'> 
+        <div className='flex justify-between'>
+          <h3 className='bg-red-700 px-3 py-1 rounded'>High</h3>
+          <h4 className='font-semibold'>20 Jan 2025</h4>
+        </div>
+
+        <h2 className='mt-5 text-xl font-semibold'> Make a new Project</h2>
+        <p className='text-sm '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, libero voluptatem. 
+          Distinctio ullam similique obcaecati dolore harum 
+          laborum non maiores temporibus illum assumenda illo quis, 
+          quisquam aut praesentium veritatis. Temporibus.
+            </p>
+
+      </div>
+
+
+      <div className='flex-shrink-0 h-full w-[300px] bg-green-400 rounded-xl p-5'> 
+        <div className='flex justify-between'>
+          <h3 className='bg-red-700 px-3 py-1 rounded'>High</h3>
+          <h4 className='font-semibold'>20 Jan 2025</h4>
+        </div>
+
+        <h2 className='mt-5 text-xl font-semibold'> Make a new Project</h2>
+        <p className='text-sm '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, libero voluptatem. 
+          Distinctio ullam similique obcaecati dolore harum 
+          laborum non maiores temporibus illum assumenda illo quis, 
+          quisquam aut praesentium veritatis. Temporibus.
+            </p>
+
+      </div>
+
+      <div className='flex-shrink-0 h-full w-[300px] bg-blue-400 rounded-xl p-5'> 
+        <div className='flex justify-between'>
+          <h3 className='bg-red-700 px-3 py-1 rounded'>High</h3>
+          <h4 className='font-semibold'>20 Jan 2025</h4>
+        </div>
+
+        <h2 className='mt-5 text-xl font-semibold'> Make a new Project</h2>
+        <p className='text-sm '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, libero voluptatem. 
+          Distinctio ullam similique obcaecati dolore harum 
+          laborum non maiores temporibus illum assumenda illo quis, 
+          quisquam aut praesentium veritatis. Temporibus.
+            </p>
+
+      </div>
+
+      <div className='flex-shrink-0 h-full w-[300px] bg-orange-400 rounded-xl p-5'> 
+        <div className='flex justify-between'>
+          <h3 className='bg-red-700 px-3 py-1 rounded'>High</h3>
+          <h4 className='font-semibold'>20 Jan 2025</h4>
+        </div>
+
+        <h2 className='mt-5 text-xl font-semibold'> Make a new Project</h2>
+        <p className='text-sm '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, libero voluptatem. 
+          Distinctio ullam similique obcaecati dolore harum 
+          laborum non maiores temporibus illum assumenda illo quis, 
+          quisquam aut praesentium veritatis. Temporibus.
+            </p>
+
+      </div>
+
+      <div className='flex-shrink-0 h-full w-[300px] bg-violet-400 rounded-xl p-5'> 
+        <div className='flex justify-between'>
+          <h3 className='bg-red-700 px-3 py-1 rounded'>High</h3>
+          <h4 className='font-semibold'>20 Jan 2025</h4>
+        </div>
+
+        <h2 className='mt-5 text-xl font-semibold'> Make a new Project</h2>
+        <p className='text-sm '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, libero voluptatem. 
+          Distinctio ullam similique obcaecati dolore harum 
+          laborum non maiores temporibus illum assumenda illo quis, 
+          quisquam aut praesentium veritatis. Temporibus.
+            </p>
+
+      </div>
+
+      
+      <div className='flex-shrink-0 h-full w-[300px] bg-black-400 rounded-xl p-5'> 
+        <div className='flex justify-between'>
+          <h3 className='bg-red-700 px-3 py-1 rounded'>High</h3>
+          <h4 className='font-semibold'>20 Jan 2025</h4>
+        </div>
+
+        <h2 className='mt-5 text-xl font-semibold'> Make a new Project</h2>
+        <p className='text-sm '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, libero voluptatem. 
+          Distinctio ullam similique obcaecati dolore harum 
+          laborum non maiores temporibus illum assumenda illo quis, 
+          quisquam aut praesentium veritatis. Temporibus.
+            </p>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default TaskList
---
+++ src/others/CreateTask.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const CreateTask = () => {
+  return (
+    <div>
+        {/* h-screen */}
+         <div className='h-90 overflow-auto w-full bg-gray-800 rounded-xl p-10 px-4 py-4'>
+              
+                <div>
+                    <form action="" className='flex flex-wrap  w-full items-start justify-between'>
+                    <div className='w-1/2'>
+                        <div>
+                            <h3 className='text-sm text-gray-500 mb-0.5'>Tasks Title</h3>
+                            <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Enter the title' />
+                        </div>
+                            
+                        <div>
+                            <h3  className='text-sm text-gray-500 mb-0.5'>Date</h3>
+                            <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date" placeholder='Enter the date' />
+                        </div>
+
+                        <div>
+                            <h3  className='text-sm text-gray-500 mb-0.5'>Assign To:</h3>
+                            <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Whom to Assign' />
+                        </div>
+
+                        <div>
+                            <h3  className='text-sm text-gray-500 mb-0.5'>Category</h3>
+                            <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Which category does it belongs to' />
+                        </div>
+
+                        </div>
+
+                        <div className='w-2/5 flex flex-col items-start'>
+                            <h3 className='text-sm text-gray-500 mb-0.5'>Description</h3>
+                            <textarea name="" id="" cols="30" rows={10} className='w-full h-55 py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray=400'></textarea>
+
+                            <button className='hover:bg-amber-400 text-sm rounded bg-amber-500 mt-4 w-full  my-5 px-5 py-3 cursor-pointer'> Create Task</button>
+                        </div>
+
+
+                    </form>
+                </div>
+
+            </div>
+
+    </div>
+  )
+}
+
+export default CreateTask
